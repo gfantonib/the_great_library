3856abbb7f442cb10746ec398611fabba8e3caa4
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-01-29 10:58:20 -0300

    main.c to check ft_printf gnl and bst

--- main.c
+++ main.c
@@ -6,11 +6,13 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 15:42:36 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/01/29 10:38:42 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/01/29 10:56:48 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/libft.h"
+#include <fcntl.h>
+
 
 int	main(void)
 {
@@ -21,5 +23,15 @@ int	main(void)
 	was_inserted = false;
 	int to_insert[] = {7, 2, 11, 5, 3};
 	for (int i = 0; i < 5; i++) tree1 = bst_insert(tree1, to_insert[i], &was_inserted);
-	bst_simple_print(tree1, 5);
+
+	int fd = open("gnl.txt", O_RDONLY);
+	char	*line;
+	while (line)
+	{
+		line = get_next_line(fd);
+		ft_printf("%s", line);
+	}
+	ft_printf("\n");
+
+	bst_breadth_first(tree1);
 }
