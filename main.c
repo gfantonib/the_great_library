ea41b28ba63773f90f8745f6456b490f6d716dcd
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-01-31 11:09:29 -0300

    bst_free_all function added to bst repository

--- main.c
+++ main.c
@@ -6,13 +6,11 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 15:42:36 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/01/29 10:56:48 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/01/31 11:00:18 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/libft.h"
-#include <fcntl.h>
-
 
 int	main(void)
 {
@@ -21,17 +19,8 @@ int	main(void)
 
 	tree1 = NULL;
 	was_inserted = false;
-	int to_insert[] = {7, 2, 11, 5, 3};
-	for (int i = 0; i < 5; i++) tree1 = bst_insert(tree1, to_insert[i], &was_inserted);
-
-	int fd = open("gnl.txt", O_RDONLY);
-	char	*line;
-	while (line)
-	{
-		line = get_next_line(fd);
-		ft_printf("%s", line);
-	}
-	ft_printf("\n");
-
-	bst_breadth_first(tree1);
+	int to_insert[] = {7, 2, 11, 5, 3, 19, 1, 8, 4};
+	for (int i = 0; i < 9; i++) tree1 = bst_insert(tree1, to_insert[i], &was_inserted);
+	bst_post_order(tree1);
+	bst_free_all(tree1);
 }
